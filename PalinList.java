4b3b868427945ec89ab0b350a16822969586982e
geode3 <132504158+geode3@users.noreply.github.com>
2025-02-28 21:00:48 -0600

    Update PalinList.java

--- PalinList.java
+++ PalinList.java
@@ -1,37 +1,70 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Giovani Zuniga
 
+/**
+ * The PalinList class determines whether a given list of space-separated words
+ * or numbers is a palindrome. It uses a queue and a stack to compare elements
+ * from both ends of the list.
+ */
 import java.util.Queue;
 import java.util.Stack;
-import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Arrays;
 
-public class PalinList
-{
-	private Queue<String> queue;
-	private Stack<String> stack;
+public class PalinList {
+    private Queue<String> queue;
+    private Stack<String> stack;
 
-	public PalinList()
-	{
-		setList("");
-	}
+    /**
+     * Default constructor that initializes the list to an empty string
+     */
+    public PalinList() {
+        setList("");
+    }
 
-	public PalinList(String list)
-	{
-	}
+    /**
+     * Constructor that initializes the list with a given string
+     * @param list The space separated string that gets checked if is palindrome
+     */
+    public PalinList(String list) {
+        setList(list);
+    }
 
-	public void setList(String list)
-	{
-	}
+    /**
+     * Sets the queue and stack with elements from the given list
+     * @param list The space separated string whose elements will be added to the queue and stack
+     */
+    public void setList(String list) {
+        queue = new LinkedList<>();
+        stack = new Stack<>();
+       
+        String[] elements = list.split(" ");
+        for (String element : elements) {
+            queue.add(element);
+            stack.push(element);
+        }
+    }
 
-	public boolean isPalin()
-	{
-		return true;
-	}
+    /**
+     * Checks whether the stored list is a palindrome
+     * @return true if the list is a palindrome, false otherwise
+     */
+    public boolean isPalin() {
+        while (!queue.isEmpty()) {
+            if (!queue.poll().equals(stack.pop())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-
-	//write a toString method
+    /**
+     * Returns a string representation of the queue
+     * @return A string representation of the stored elements in the queue
+     */
+    public String toString() {
+        return queue.toString();
+    }
 }
